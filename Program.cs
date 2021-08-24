9ee44df35d438e5fc4f2edb343e7daa36af03648
zimmeste <Stefan.Zimmermann@infineon.com>
2021-08-24 16:27:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            { 
+                Console.WriteLine("Hello World!");
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
